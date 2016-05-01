a88276702dab962db80220ee67423bb45dc515a7
Kenchu123 <k88913n@gmail.com>
2016-05-01 23:04:01 +0800

    third

--- materialize/js/init.js
+++ materialize/js/init.js
@@ -1,8 +1,6 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  <script type="text/javascript">
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+  </script>
